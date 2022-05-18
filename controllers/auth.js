e65be3dac08d6aee5e25e3b8213f7a5dcda611cd
a1a2a3a4a5 <linub7@gmail.com>
2022-05-18 12:19:29 +0430

    Add login & register routes and controllers and verify middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,88 @@
-exports.register = (req, res) => {
-  res.send('register');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  try {
+    const {
+      body: { username, password, email },
+    } = req;
+
+    // check if user exist
+    const existUser = await User.exists({ email });
+    if (existUser)
+      return res.status(409).json({ message: 'Email already in use' }); // 409: conflict statusCode
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    // Create user document and save in db
+    const newUser = await User.create({
+      username,
+      email: email.toLowerCase(),
+      password: hashedPassword,
+    });
+
+    // create JWT token
+    const token = jwt.sign(
+      {
+        userId: newUser._id,
+        email,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '24h' }
+    );
+
+    res.status(201).json({
+      userDetails: {
+        email: newUser.email,
+        token,
+        username: newUser.username,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err.message });
+  }
 };
 
-exports.login = (req, res) => {
-  res.send('login');
+exports.login = async (req, res) => {
+  try {
+    const {
+      body: { password, email },
+    } = req;
+
+    // check if user does'nt exist
+    const user = await User.findOne({ email: email.toLowerCase() });
+    if (!user)
+      return res
+        .status(400)
+        .json({ message: 'Wrong Credential, please try again' });
+
+    // decode password
+    const matched = await bcrypt.compare(password, user.password);
+    if (!matched) {
+      return res
+        .status(400)
+        .json({ message: 'Wrong Credential, please try again' });
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        email,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '24h' }
+    );
+    return res.status(200).json({
+      userDetails: {
+        email: user.email,
+        token,
+        username: user.username,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err.message });
+  }
 };
--- middleware/authValidation.js
+++ middleware/authValidation.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
 const Joi = require('joi');
+
+const config = process.env;
 
 exports.registerSchema = Joi.object({
   username: Joi.string().min(3).max(12).required(),
@@ -10,3 +13,20 @@ exports.loginSchema = Joi.object({
   password: Joi.string().min(6).max(12).required(),
   email: Joi.string().email().required(),
 });
+
+exports.verifyToken = async (req, res, next) => {
+  let token = req.body.token || req.query.token || req.headers['authorization'];
+
+  if (!token)
+    return res
+      .status(403)
+      .json({ message: 'Token is required for authentication' });
+  try {
+    token = token.replace(/^Bearer\s+/, '');
+    const decoded = jwt.verify(token, config.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid Token' });
+  }
+};
